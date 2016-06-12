fca4778bea0459b8b56a258353176d986f808a87
thyrusg <me@thyrusgorges.com>
2016-06-12 15:21:16 -0700

    Added the basic routes for the application

--- route.py
+++ route.py
@@ -1,3 +1,22 @@
 #!/bin/bash/env python 
 
-print "Hello World"
+import requests
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "Hello World"
+	
+@app.route('/lookup', methods=['POST'])
+def lookup():
+		return "Phone Number"
+
+@app.route('/results')
+def results():
+	return "Results go here"
+	
+
+if __name__=='__main__':
+	app.run()
